92f0288bc5e687f7a1a25bbf336de615db4a6530
enieber cunha <eniebercunha@gmail.com>
2022-10-19 19:45:34 -0300

    feat: first commit of account

---
+++ src/account.rs
@@ -0,0 +1,71 @@
+pub mod account {
+    mod document {
+        #[derive(Debug)]
+        #[derive(PartialEq)]
+        pub struct Document {
+            str: String,
+        }
+
+        impl Document {
+            pub fn new(str: String) -> Option<Self> {
+                if str.chars().count() <= 1 {
+                    None
+                } else {
+                    Some(Document { str })
+                }
+            }
+        }
+    }
+
+    #[derive(Debug)]
+    #[derive(PartialEq)]
+    struct Owner {
+        name: String,
+        document: document::Document,
+    }
+
+    #[derive(Debug)]
+    #[derive(PartialEq)]
+    pub struct Account {
+        amount: f64,
+        owner: Owner,
+    }
+
+    impl ToString for Account {
+        fn to_string(&self) -> String {
+            return format!("The Owner {} has amount {}", &self.owner.name, &self.amount);
+        }
+    }
+
+    pub fn open_account(
+        namer_owner: String,
+        document: String,
+        amount: f64,
+    ) -> Result<Account, String> {
+        match document::Document::new(document) {
+            Some(doc) => {
+                let personal = Owner {
+                    name: namer_owner,
+                    document: doc,
+                };
+                return Ok(Account {
+                    amount: amount,
+                    owner: personal,
+                });
+            }
+            None => return Err(String::from("Document invalid")),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+      
+    #[test]
+    fn invalid_document_in_open_account() {
+        let my_account = account::open_account(String::from("Enieber Cunha"), String::from("1"), 10.90);
+        assert_eq!(my_account.unwrap(), Err(String::from("Document invalid")))
+    }
+}
+
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+mod account;
+use crate::account::account::open_account;
+
+fn main() {
+    let my_account = open_account(String::from("Enieber Cunha"), String::from("123"), 10.90);
+    match my_account {
+        Ok(account) => println!("{}", account.to_string()),
+        Err(err) => println!("{}", err),
+    }
+
+    let outher_account = open_account(String::from("Marcelo "), String::from("321"), 90.60);
+    match outher_account {
+        Ok(account) => println!("{}", account.to_string()),
+        Err(err) => println!("{}", err),
+    }
+
+    let invalid_account = open_account(String::from("Marcelo "), String::from("1"), 90.60);
+    match invalid_account {
+        Ok(account) => println!("{}", account.to_string()),
+        Err(err) => println!("{}", err),
+    }
+}
