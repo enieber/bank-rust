5b9c9a7f8d3504f47bdb61dd9e2f3b63926cb5b3
enieber cunha <eniebercunha@gmail.com>
2022-10-20 12:05:18 -0300

    feat: create test to open_account

--- src/account.rs
+++ src/account.rs
@@ -65,7 +65,14 @@ mod tests {
     #[test]
     fn invalid_document_in_open_account() {
         let my_account = account::open_account(String::from("Enieber Cunha"), String::from("1"), 10.90);
-        assert_eq!(my_account.unwrap(), Err(String::from("Document invalid")))
+        assert_eq!(my_account, Err(String::from("Document invalid")))
     }
+ 
+    #[test]
+    fn open_account_with_success() {
+        let my_account = account::open_account(String::from("Enieber Cunha"), String::from("123"), 10.90);
+        assert_eq!(my_account.unwrap().to_string(), String::from("The Owner Enieber Cunha has amount 10.9"))
+    }
+
 }
 
